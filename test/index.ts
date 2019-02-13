58225113df949423da430bfce3ce82a98029b180
Andres Rios <rianby64@gmail.com>
2019-02-13 22:14:02 +0300

    Define initPage function

--- test/index.ts
+++ test/index.ts
@@ -1,14 +1,18 @@
 
 import * as test from 'tape';
-import { JSDOM } from 'jsdom';
+import { JSDOM, DOMWindow } from 'jsdom';
+
+function initPage(): Promise<{ window: DOMWindow, document: Document }> {
+  return JSDOM.fromFile('./src/index.html').then((dom: JSDOM) => ({
+    window: dom.window,
+    document: dom.window.document
+  }));
+}
 
 test('Doing something interesting', (t: test.Test) => {
   t.plan(1);
 
-  JSDOM.fromFile('./src/index.html').then((dom: JSDOM) => {
-    const { window } = dom;
-    const { document } = window;
-
+  initPage().then(({ window, document }) => {
     t.ok(document.querySelector('div').tagName === 'DIV', 'ok');
   });
 });
