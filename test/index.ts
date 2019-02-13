0fbed308a28786ec6bd78482adfd82af0c8597df
Andres Rios <rianby64@gmail.com>
2019-02-14 00:16:17 +0300

    Ok...

--- test/index.ts
+++ test/index.ts
@@ -2,17 +2,16 @@
 import * as test from 'tape';
 import { JSDOM, DOMWindow } from 'jsdom';
 
-function initPage(): Promise<{ window: DOMWindow, document: Document }> {
-  return JSDOM.fromFile('./src/index.html').then((dom: JSDOM) => ({
+const initPage = (): Promise<{ window: DOMWindow, document: Document }> =>
+  JSDOM.fromFile('./src/index.html').then((dom: JSDOM) => ({
     window: dom.window,
     document: dom.window.document
   }));
-}
 
 test('Doing something interesting', (t: test.Test) => {
   t.plan(1);
 
-  initPage().then(({ window, document }) => {
+  initPage().then(({ document }) => {
     t.ok(document.querySelector('div').tagName === 'DIV', 'ok');
   });
 });
