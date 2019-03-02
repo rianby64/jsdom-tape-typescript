f2b8ce9efae50b56228524cf242d3065db6fc130
Andres Rios <rianby64@gmail.com>
2019-03-02 14:15:28 +0300

    Just fun. Redefine the args

--- test/index.ts
+++ test/index.ts
@@ -2,52 +2,47 @@
 import * as test from 'tape'
 import { JSDOM, DOMWindow } from 'jsdom'
 
-// initPage function allows to load the page within a promise
-const initPage = (): Promise<{ document: Document, window: DOMWindow }> =>
+interface JustFun {
+  document: Document,
+  window: DOMWindow,
+}
+
+const justFun = ((fn: Function) => {
   JSDOM.fromFile('./src/index.html', {
     runScripts: 'dangerously',
     resources: 'usable'
   })
-    .then((dom: JSDOM) => {
-      return {
-        window: dom.window,
-        document: dom.window.document
-      }
-    })
+    .then((dom: JSDOM) => setTimeout(() =>
+      fn({ document: dom.window.document, window: dom.window }), 0))
+    .catch(reason => console.error(reason))
+})
 
 // Test example
 test('Doing something interesting',(t: test.Test) => {
   t.plan(1)
 
-  initPage()
-    .then(({ window, document }) => {
-      const div = document.querySelector('div')
+  justFun(({ document, window }: JustFun) => {
+    const div = document.querySelector('div')
 
-      t.ok(div instanceof window.HTMLDivElement, 'div is present')
-    })
-    .catch((reason: any) => {
-      console.error(reason)
-    })
+    t.ok(div instanceof window.HTMLDivElement, 'div is present')
+  })
 })
 
 // Test example
 test('Click pressed',(t: test.Test) => {
   t.plan(3)
 
-  initPage()
-    .then(({ window, document }) => {
-      setTimeout(() => {
-        const button = document.querySelector('button')
-        const span = document.querySelector('span')
+  justFun(({ document, window }: JustFun) => {
+    const button = document.querySelector('button')
+    const span = document.querySelector('span')
 
-        t.ok(span instanceof window.HTMLSpanElement, 'span is present')
-        t.ok(button instanceof window.HTMLButtonElement, 'button is present')
+    t.ok(span instanceof window.HTMLSpanElement, 'span is present')
+    t.ok(button instanceof window.HTMLButtonElement, 'button is present')
 
-        button && button.click()
-        t.equal(span && span.textContent, 'pressed', 'button triggers span.textContent = pressed')
-      }, 0)
-    })
-    .catch(reason => {
-      console.error(reason)
-    })
+    button && button.click()
+    t.equal(
+      span && span.textContent,
+      'pressed',
+      'button triggers span.textContent = pressed')
+  })
 })
